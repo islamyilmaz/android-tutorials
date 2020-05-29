a11bd417bb1347187c2f7f1b94170f984eea96d1
İslam YILMAZ <reqislam@gmail.com>
2020-05-29 11:25:26 +0300

    goto maps-store-twitter --> button

--- Example003_IntentsGoogleMap/app/src/main/java/com/islamyilmaz/example003_intentsgooglemap/MainActivity.java
+++ Example003_IntentsGoogleMap/app/src/main/java/com/islamyilmaz/example003_intentsgooglemap/MainActivity.java
@@ -2,13 +2,46 @@ package com.islamyilmaz.example003_intentsgooglemap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    Intent intent = null , chooser = null ;
+    public void buttonPressed(View v){
+        if(v.getId() == R.id.buttonGotoLocation){
+            Log.i("islam","Location button pressed");
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:39.925002, 32.836840"));
+            chooser = Intent.createChooser(intent, "Launch Maps");
+            startActivity(chooser);
+        }
+
+        if(v.getId() == R.id.buttonApp){
+            Log.i("islam","App button pressed");
+            intent= new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("market://details?id=tr.gov.saglik.hayatevesigar"));
+            chooser = Intent.createChooser(intent,"Launch App");
+            startActivity(chooser);
+        }
+
+        if(v.getId() == R.id.buttonSocialMedia){
+            Log.i("islam","Socialmedia button pressed");
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://twitter.com/csnaber"));
+            chooser = Intent.createChooser(intent,"Launch Twitter");
+            startActivity(chooser);
+        }
     }
 }
--- README.md
+++
@@ -1,6 +0,0 @@
-# Basic Android Tutorials
-
-  - Example000 : Button onClickListener 
-  - Example001 : Save and Restore Instance State 
-  - Example002 : Open an Activity with Intent
-  - Example003 : Send email with intent
